b7b522f274d8c4efd403ad8a947ee05f4c496d61
Sebastian Paulo <sebastian.glass15@gmail.com>
2026-02-17 16:14:54 +0100

    fix: temp fix for linting

--- src/app/shared/loading-spinner/loading-spinner.component.ts
+++ src/app/shared/loading-spinner/loading-spinner.component.ts
@@ -6,6 +6,4 @@ import { Component } from '@angular/core';
   templateUrl: './loading-spinner.component.html',
   styleUrl: './loading-spinner.component.scss',
 })
-export class LoadingSpinnerComponent {
-
-}
+export class LoadingSpinnerComponent {}
